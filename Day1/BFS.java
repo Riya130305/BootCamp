7577812f07019da0e6b1b7d14349bd612a630ad6
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2025-12-30 01:44:19 +0530

    BFS

--- Day1/BFS.java
+++ Day1/BFS.java
@@ -1,45 +1,37 @@
 import java.util.*;
 public class BFS {
     public static void main(String[] args) {
-         ArrayList<ArrayList<Integer>> adj = new ArrayList<>(
-    Arrays.asList(
-        new ArrayList<>(Arrays.asList(1, 2)),
-        new ArrayList<>(Arrays.asList(0, 2)),
-        new ArrayList<>(Arrays.asList(0, 1, 3, 4)),
-        new ArrayList<>(Arrays.asList(2)),
-        new ArrayList<>(Arrays.asList(2))
-    )
-);
-        bfs(adj);
-
-    
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(
+            Arrays.asList(
+                new ArrayList<>(Arrays.asList(1, 2)),
+                new ArrayList<>(Arrays.asList(0, 2)),
+                new ArrayList<>(Arrays.asList(0, 1, 3, 4)),
+                new ArrayList<>(Arrays.asList(2)),
+                new ArrayList<>(Arrays.asList(2))
+            )
+        );
+        System.out.println(bfs(adj)); // prints traversal
     }
-    public static ArrayList<Integer> bfs( ArrayList<ArrayList<Integer>> adj )
-    {
-        ArrayList<Integer>arr = new ArrayList<>();
-        Queue<Integer> q= new LinkedList<>() ;
-        boolean visited[]= new boolean[adj.size()];
-        int node=0;
-        fun(adj,arr,q,node,visited);
-        return arr;
-    } 
-    public static void fun(ArrayList<ArrayList<Integer>> adj,ArrayList<Integer>ans, Queue<Integer> q,int node, boolean visited[])
-    {
-        visited[node]=true;
-        q.add(node);
-        while(!q.isEmpty()){
-            int poll=q.poll();
-            ans.add(poll);
-        for(int nbr:adj.get(poll))
-        {
-            if(!visited[nbr])
-            {
-                q.add(nbr);
-                fun(adj, ans, q, node, visited);
+
+    public static ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> order = new ArrayList<>();
+        boolean[] visited = new boolean[adj.size()];
+        Queue<Integer> q = new LinkedList<>();
+
+        int start = 0;
+        visited[start] = true;
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            order.add(u);
+            for (int v : adj.get(u)) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.add(v);
+                }
             }
         }
+        return order;
     }
-
-    }
-    
 }
